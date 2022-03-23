71e83f0f755eccc0a7b75f76c43c443753ac9467
Callum Dunster <cdunster@users.noreply.github.com>
2022-03-23 18:44:48 +0100

    Add more comments.
    Signed-off-by: Callum Dunster <cdunster@users.noreply.github.com>

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,3 +1,4 @@
+/// Function to be called externally from C code.
 #[no_mangle]
 extern "C" fn greet() {
     println!("Hello from Rust code!");
--- src/main.c
+++ src/main.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
+// This is the Rust function to be called.
 void greet(void);
 
+// Main entry point of the executable that will call Rust code.
 int main() {
     greet();
     printf("Hello from C code!");
